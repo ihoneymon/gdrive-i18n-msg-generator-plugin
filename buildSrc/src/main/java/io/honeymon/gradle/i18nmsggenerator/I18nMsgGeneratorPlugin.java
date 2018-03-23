73e8e2fa7b13044ee98a70663d7a164f7381ec48
honeymon <ihoneymon@gmail.com>
2018-03-23 19:49:17 +0900

    plugin 소스를 생성하는 위치를 알게 되었다. `buildSrc/src/main/java`

---
+++ buildSrc/src/main/java/io/honeymon/gradle/i18nmsggenerator/I18nMsgGeneratorPlugin.java
@@ -0,0 +1,11 @@
+package io.honeymon.gradle.i18nmsggenerator;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class I18nMsgGeneratorPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        //TODO Implementations
+    }
+}
